b5bad1caa04aa72bbf3d35aa7723d1117400b04d
quakeroatsgod <agnellogriffen@gmail.com>
2022-11-06 17:21:47 -0800

    set up webhook. Just a temp commit

--- src/noodlearm/Grid.java
+++ src/noodlearm/Grid.java
@@ -16,13 +16,13 @@ public class Grid extends Entity{
         this.travel_cost=Integer.MAX_VALUE;
     }
     //Regular grid tile in generation
-    public Grid(String texture, int x_pos, int y_pos, int ID){
+    public Grid(String texture_type, int x_pos, int y_pos, int ID){
         //Multiply grid's position with the initializor iterator plus a screen offset.
         super(x_pos*32+24,y_pos*32+64);
         this.entity="";
         this.ID=ID;
-        this.type=texture;
-        switch(texture){
+        this.type=texture_type;
+        switch(texture_type){
             case "WALL":
                 //addImageWithBoundingBox(ResourceManager.getImage(Pushover.WALL_RES));
                 this.walkable=false;
@@ -37,6 +37,28 @@ public class Grid extends Entity{
                 this.travel_cost=1.0f;
                 break;
         }
+    }
 
+    public int getID(){
+        return this.ID;
+    }
+    public void setID(int ID){
+        this.ID=ID;
+    }
+
+    /**
+     * Sets Sets the entity of the grid tile
+     * @param entity
+     */
+    public void setEntity(String entity){
+        this.entity=entity;
+    }
+
+    public String getEntity(){
+        return this.entity;
+    }
+    
+    public String getType(){
+        return this.type;
     }
 }
