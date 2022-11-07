fc3d1431d7973194342abc27434d4bef699a5e5b
quakeroatsgod <agnellogriffen@gmail.com>
2022-11-06 16:47:04 -0800

    Finished issue 1. Made skeletal game frame

--- src/noodlearm/GameoverState.java
+++ src/noodlearm/GameoverState.java
@@ -11,15 +11,14 @@ import org.newdawn.slick.state.transition.EmptyTransition;
 import org.newdawn.slick.state.transition.HorizontalSplitTransition;
 import jig.Vector;
 
-public class StartupState extends BasicGameState {
+public class GameoverState extends BasicGameState {
     @Override
     public int getID() {
-        return Noodlearm.STARTUPSTATE;
+        return Noodlearm.GAMEOVERSTATE;
     }
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
-
     }
 
     @Override
@@ -28,16 +27,12 @@ public class StartupState extends BasicGameState {
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         //TODO
-        //g.drawImage(ResourceManager.getImage(Noodlearm.STARTUP_SCREEN_RES), 0, 0);
+        //g.drawImage(ResourceManager.getImage(Noodlearm.GAMEOVER_SCREEN_RES), 0, 0);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
         Input input = container.getInput();
         Noodlearm na = (Noodlearm)game;
-		//Await user input to start the game
-		if (input.isKeyDown(Input.KEY_SPACE)){ 	
-            na.enterState(Noodlearm.PLAYINGSTATE, new EmptyTransition(), new HorizontalSplitTransition());
-        }
     }
 }
